7fdb1f7f5f814b153d41e65d9e810e9a74d1f602
abounaasi <ahmadabounaasi@gmail.com>
2026-05-04 18:51:27 +0300

    added otp verify account

--- frontend/@latest/src/context/UserContext.jsx
+++ frontend/@latest/src/context/UserContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+/*import { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { server } from "../main";
 import toast, { Toaster } from "react-hot-toast";
@@ -12,6 +12,7 @@ export const UserContextProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
 
   async function loginUser(email, password, navigate) {
+    setBtnLoading(true);
     try {
       const { data } = await axios.post(`${server}/api/user/login`, {
         email,
@@ -27,6 +28,25 @@ export const UserContextProvider = ({ children }) => {
     } catch (error) {
       setBtnLoading(false);
       setIsAuth(false);
+      toast.error(error.response.data.message);
+    }
+  }
+
+  async function registerUser(name, email, password, navigate) {
+    setBtnLoading(true);
+    try {
+      const { data } = await axios.post(`${server}/api/user/register`, {
+        name,
+        email,
+        password,
+      });
+
+      toast.success(data.message);
+      localStorage.setItem("activationToken", data.activationToken);
+      setBtnLoading(false);
+      navigate("/verify");
+    } catch (error) {
+      setBtnLoading(false);
       toast.error(error.response.data.message);
     }
   }
@@ -61,6 +81,8 @@ export const UserContextProvider = ({ children }) => {
         loginUser,
         btnLoading,
         loading,
+        fetchUser,
+        registerUser,
       }}
     >
       {children}
@@ -68,4 +90,121 @@ export const UserContextProvider = ({ children }) => {
     </UserContext.Provider>
   );
 };
+export const UserData = () => useContext(UserContext);
+*/
+
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { server } from "../main";
+import toast, { Toaster } from "react-hot-toast";
+
+const UserContext = createContext();
+
+export const UserContextProvider = ({ children }) => {
+  const [user, setUser] = useState([]);
+  const [isAuth, setIsAuth] = useState(false);
+  const [btnLoading, setBtnLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  async function loginUser(email, password, navigate, fetchMyCourse) {
+    setBtnLoading(true);
+    try {
+      const { data } = await axios.post(`${server}/api/user/login`, {
+        email,
+        password,
+      });
+
+      toast.success(data.message);
+      localStorage.setItem("token", data.token);
+      setUser(data.user);
+      setIsAuth(true);
+      setBtnLoading(false);
+      navigate("/");
+      fetchMyCourse();
+    } catch (error) {
+      setBtnLoading(false);
+      setIsAuth(false);
+      toast.error(error.response.data.message);
+    }
+  }
+
+  async function registerUser(name, email, password, navigate) {
+    setBtnLoading(true);
+    try {
+      const { data } = await axios.post(`${server}/api/user/register`, {
+        name,
+        email,
+        password,
+      });
+
+      toast.success(data.message);
+      localStorage.setItem("activationToken", data.activationToken);
+      setBtnLoading(false);
+      navigate("/verify");
+    } catch (error) {
+      setBtnLoading(false);
+      toast.error(error.response.data.message);
+    }
+  }
+
+  async function verifyOtp(otp, navigate) {
+    setBtnLoading(true);
+    const activationToken = localStorage.getItem("activationToken");
+    try {
+      const { data } = await axios.post(`${server}/api/user/verify`, {
+        otp,
+        activationToken,
+      });
+
+      toast.success(data.message);
+      navigate("/login");
+      localStorage.clear();
+      setBtnLoading(false);
+    } catch (error) {
+      toast.error(error.response.data.message);
+      setBtnLoading(false);
+    }
+  }
+
+  async function fetchUser() {
+    try {
+      const { data } = await axios.get(`${server}/api/user/me`, {
+        headers: {
+          token: localStorage.getItem("token"),
+        },
+      });
+
+      setIsAuth(true);
+      setUser(data.user);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        setUser,
+        setIsAuth,
+        isAuth,
+        loginUser,
+        btnLoading,
+        loading,
+        registerUser,
+        verifyOtp,
+        fetchUser,
+      }}
+    >
+      {children}
+      <Toaster />
+    </UserContext.Provider>
+  );
+};
+
 export const UserData = () => useContext(UserContext);
--- frontend/@latest/src/pages/auth/Verify.jsx
+++ frontend/@latest/src/pages/auth/Verify.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./auth.css";
 import { Link } from "react-router-dom";
 
 const Verify = () => {
+  const [otp, setOtp] = useState("");
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(otp);
+  };
   return (
     <div className="auth-page">
       <div className="auth-form">
         <h2>Verify Account</h2>
-        <form>
+        <form onSubmit={submitHandler}>
           <label htmlFor="otp">Otp</label>
-          <input type="number" required />
-          <button className="common-btn">Verify</button>
+          <input
+            type="number"
+            value={otp}
+            onChange={(e) => setOtp(e.target.value)}
+            required
+          />
+          <button type="submit" className="common-btn">
+            Verify
+          </button>
         </form>
         <p>
           Go to <Link to="/login">Login</Link> page
